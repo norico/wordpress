53b2e647b260cc304378274cb1aa85398d81733d
norico <legion.norico@gmail.com>
2024-04-04 21:57:42 +0000

    Initial Commit

---
+++ wp-content/plugins/intranet/src/Intranet.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Intranet;
+
+class Intranet {
+
+	private string $plugin_filename;
+
+	public function __construct($plugin_filename) {
+		$this->plugin_filename = $plugin_filename;
+	}
+
+	public function run() {
+
+	}
+}
---
+++ wp-content/plugins/intranet/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'norico/intranet',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd3c46ae3f0584d0706a90bef7880b7f823e0301e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'norico/intranet' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd3c46ae3f0584d0706a90bef7880b7f823e0301e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
