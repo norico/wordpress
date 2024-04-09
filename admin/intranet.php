9eb76330d139bce2cb30ee88564ba4a76f0553e9
norico <legion.norico@gmail.com>
2024-04-09 22:30:25 +0200

    Feature: Add menu page Feature: Add settings page

---
+++ admin/intranet.php
@@ -0,0 +1,2 @@
+<?php
+echo 'This is intranet file in admin directory.';
--- src/Plugin.php
+++ src/Plugin.php
@@ -12,6 +12,11 @@ class Plugin
     private string $domainpath;
     private string $wordpress;
     private string $php;
+    private string $dir;
+    private string $url;
+    private string $basename;
+    private string $slug;
+
     private bool $network;
 
     /**
@@ -22,6 +27,11 @@ class Plugin
         if( ! function_exists('get_plugin_data') ){
             require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
         }
+
+        $this->dir = plugin_dir_path($plugin_filename);
+        $this->url = plugin_dir_url($plugin_filename);
+        $this->basename = plugin_basename($plugin_filename);
+        $this->slug = dirname(plugin_basename($plugin_filename));
         $plugin_data = get_plugin_data($plugin_filename);
         if($plugin_data) {
             $this->name        = $plugin_data['Name'];
@@ -34,6 +44,88 @@ class Plugin
             $this->wordpress   = $plugin_data['RequiresWP'];
             $this->php         = $plugin_data['RequiresPHP'];
         }
+        $this->create_admin_menu();
+
+    }
+
+    private function create_admin_menu()
+    {
+        if ( is_multisite() && $this->isNetwork() ){
+            add_filter('network_admin_plugin_action_links_'.$this->getBasename(), [$this, 'add_action_links']);
+            add_action('network_admin_menu', [$this, 'admin_menu'], 10);
+        }
+        else {
+            add_filter('plugin_action_links_'.$this->getBasename(), [$this, 'add_action_links']);
+            add_action('admin_menu', [$this, 'admin_menu'], 10);
+        }
+    }
+
+
+    public function add_action_links( array $links ): array {
+        $admin_url  = (is_multisite() &&  $this->isNetwork() ) ? "network_admin_url" : "admin_url";
+        $admin_path = (is_multisite() && $this->isNetwork() )  ? "network.php" : "admin.php";
+        $links[] = sprintf("<a href='%s'>%s</a>", $admin_url( $admin_path.'?page='.$this->getSlug() ), __('Settings', 'default'));
+        return $links;
+    }
+
+    public function admin_menu(): void {
+        $args = array(
+            'network' => $this->isNetwork(),
+            'icon' => 'dashicons-admin-multisite',
+            'position' => 90,
+        );
+        $this->add_admin_menu( $args );
+    }
+
+    public function add_admin_menu( array $args ) {
+        $defaults = array(
+            'network' => false,
+            'icon' => 'dashicons-admin-generic',
+            'position' => 90,
+        );
+        $args = wp_parse_args( $args, $defaults );
+        $admin_page = add_menu_page($this->getName(), $this->getName(), 'manage_options', $this->getSlug(), [$this, 'render_admin_page'], $args['icon'], $args['position']);
+    }
+
+    public function render_admin_page(): void {
+        $filename = $this->getDir().'admin/'.$this->getSlug().'.php';
+        echo '<div class="wrap">';
+        printf('<h1>%s</h1><hr/>', get_admin_page_title() );
+        if ( file_exists($filename) )
+        {
+            require_once($filename);
+        }
+        else {
+            printf('<p>%s</p>', __('Missing admin page', 'intranet') );
+            printf('<p>%s</p>', __('Please check your file intranet.php, in /admin directory', 'intranet'));
+        }
+        echo '</div>';
+    }
+
+
+
+
+
+
+
+    public function getBasename(): string
+    {
+        return $this->basename;
+    }
+
+    public function getDir(): string
+    {
+        return $this->dir;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
     }
 
     public function getName(): string
